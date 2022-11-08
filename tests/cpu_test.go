3483963a5905ef21f9764441250eb89100970284
Chipskein <abfn0905@gmail.com>
2022-11-07 22:28:26 -0300

    arrumando test

--- tests/cpu_test.go
+++ tests/cpu_test.go
@@ -4,18 +4,30 @@ import (
 	"chip8/internal/cpu"
 	"fmt"
 	"io/ioutil"
+	"log"
 
 	"testing"
 )
 
-func TestCpu(t *testing.T) {
+var files []string
+
+func init() {
 	const assets_path = "./assets/roms"
 	dir, _ := ioutil.ReadDir(assets_path)
 	for _, file := range dir {
 		s := fmt.Sprintf("%s/%s", assets_path, file.Name())
-		fmt.Printf("Test ROM :%s\n", s)
-		romData, _ := ioutil.ReadFile(s)
-		cpu.Init(romData)
+		files = append(files, s)
 	}
 
+}
+
+func TestCpu(t *testing.T) {
+	for _, rom_path := range files {
+		data, err := ioutil.ReadFile(rom_path)
+		if err != nil {
+			log.Printf("Fail to read ROM :%s\n", rom_path)
+		}
+		log.Printf("Loaded ROM :%s\nRom Size:%d Bytes\n", rom_path, uint16(len(data)))
+		cpu.Init(data)
+	}
 }
