aedc53e3926644e86b255cf5f56e4080307a2645
Chipskein <abfn0905@gmail.com>
2022-11-07 22:48:02 -0300

    .

--- internal/cpu/cpu_test.go
+++ internal/cpu/cpu_test.go
@@ -4,6 +4,7 @@ import (
 	"chip8/internal/cpu"
 	"fmt"
 	"io/ioutil"
+	"log"
 
 	"testing"
 )
@@ -20,13 +21,13 @@ func init() {
 
 }
 
-func TestCPUInit(t *testing.T) {
+func TestInit(t *testing.T) {
 	for _, rom_path := range files {
 		data, err := ioutil.ReadFile(rom_path)
 		if err != nil {
-			fmt.Printf("Fail to read ROM :%s\n", rom_path)
+			log.Printf("Fail to read ROM :%s\n", rom_path)
 		}
-		fmt.Printf("Loaded ROM :%s\nRom Size:%d Bytes\n", rom_path, uint16(len(data)))
+		log.Printf("Loaded ROM :%s\nRom Size:%d Bytes\n", rom_path, uint16(len(data)))
 		cpu.Init(data)
 	}
 }
