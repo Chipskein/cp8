375f661bf7bf177dbf2c93053170c32a7a7f2383
Chipskein <abfn0905@gmail.com>
2022-11-13 22:56:28 -0300

    removing defer

--- internal/screen/screen.go
+++ internal/screen/screen.go
@@ -12,36 +12,34 @@ func Init() {
 		log.Printf("%s\n", err)
 		panic(err)
 	}
-	defer sdl.Quit()
+	//defer sdl.Quit()
 
 	window, err := sdl.CreateWindow("test", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, 200, 200, sdl.WINDOW_SHOWN)
 	if err != nil {
 		log.Printf("%s\n", err)
 		panic(err)
 	}
-	defer window.Destroy()
+	//defer window.Destroy()
 
-	//surface , err := window.GetSurface()
-	//if err != nil {
-	//	panic(err)
-	//}
-	//surface.FillRect(nil, 0)
+	surface, err := window.GetSurface()
+	if err != nil {
+		panic(err)
+	}
+	surface.FillRect(nil, 0)
 
-	//rect := sdl.Rect{0, 0, 200, 200}
-	//surface.FillRect(&rect, 0xffff0000)
-	//window.UpdateSurface()
-	/*
-	   running := true
+	rect := sdl.Rect{0, 0, 200, 200}
+	surface.FillRect(&rect, 0xffff0000)
+	window.UpdateSurface()
+	running := true
 
-	   	for running {
-	   		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
-	   			switch event.(type) {
-	   			case *sdl.QuitEvent:
-	   				println("Quit")
-	   				running = false
-	   				break
-	   			}
-	   		}
-	   	}
-	*/
+	for running {
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch event.(type) {
+			case *sdl.QuitEvent:
+				println("Quit")
+				running = false
+				break
+			}
+		}
+	}
 }
