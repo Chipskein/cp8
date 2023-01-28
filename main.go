6302a50e1aab1048d6493e7b1555e2045da465c3
Chipskein <abfn0905@gmail.com>
2023-01-28 14:12:18 -0300

    window

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func initSDL() (window *sdl.Window, err error) {
 	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
 		return nil, err
 	}
-	window, err = sdl.CreateWindow("chip8", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, 400, 400, sdl.WINDOW_SHOWN)
+	window, err = sdl.CreateWindow("chip8", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, 640, 320, sdl.WINDOW_SHOWN)
 	if err != nil {
 		return nil, err
 	}
@@ -33,10 +33,23 @@ func main() {
 		panic(err)
 	}
 	defer window.Destroy()
+
+	renderer, err := sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
+	if err != nil {
+		panic(err)
+	}
+	defer renderer.Destroy()
+
 	running := true
 	for running {
 		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
 			handleSDLEvents(event, &running)
 		}
+		renderer.SetDrawColor(0, 0, 0, 255)
+		renderer.Clear()
+		renderer.SetDrawColor(255, 255, 255, 255)
+
+		renderer.Present()
+		sdl.Delay(16)
 	}
 }
