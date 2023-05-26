fa1280f0d74c027aeb267033ebc2ddf9ae2b2798
Chipskein <abfn0905@gmail.com>
2023-05-26 04:21:51 -0300

    fix:fatal at main

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	var rom_path = "./internal/__test_roms__/IBM_Logo.ch8"
 	data, err := ioutil.ReadFile(rom_path)
 	if err != nil {
-		log.Printf("Fail to read ROM :%s\n", rom_path)
+		log.Fatalf("Fail to read ROM :%s\n", rom_path)
 	}
 	log.Printf("Loaded ROM :%s\nRom Size:%d Bytes\n", rom_path, uint16(len(data)))
 	cpu.Init(data)
